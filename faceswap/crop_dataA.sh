0b0c8765ad34c09653a43ddf796e7ea063ac1633
Jaerock Kwon <jaerock.kwon@gmail.com>
2019-02-28 12:16:04 +0900

    clean up

--- faceswap/crop_dataA.sh
+++
@@ -1,1 +0,0 @@
-mogrify -crop 512x512+384+0 -path ./datasets/dataA/taeri_cropped2 ./datasets/dataA/taeri/*.png 
--- faceswap/crop_dataB.sh
+++
@@ -1,1 +0,0 @@
-mogrify -crop 512x512+384+0 -path ./datasets/dataB/iu_cropped2 ./datasets/dataB/iu/*.png 
