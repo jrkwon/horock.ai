dac5009f523a9ac5f08380b4a927bc834e916ec0
Jaerock Kwon <jkwon@kettering.edu>
2019-03-03 22:09:06 -0500

    fix data path

--- scripts/copy_test_images.sh
+++ scripts/copy_test_images.sh
@@ -15,12 +15,10 @@ then
     NAME=$NAME_B
 fi
  
-PROJECT_LOC=../faceswap
-SRC_DATA_LOC=$PROJECT_LOC/datasets/data$AB/$NAME-faces-test
+SRC_DATA_LOC=datasets/$NAME-faces-test
 
 NAMES=$NAME_A-$NAME_B
-PROJECT_LOC=../Recycle-GAN
-DST_DATA_LOC=$PROJECT_LOC/datasets/faces/$NAMES/test$AB
+DST_DATA_LOC=datasets/$NAMES/test$AB
 
 mkdir -p $DST_DATA_LOC
 cp $SRC_DATA_LOC/*.png $DST_DATA_LOC/.
