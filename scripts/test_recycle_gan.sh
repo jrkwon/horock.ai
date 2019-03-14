2c9d2bae934f2b3a1e4d9a9de038e844403912a1
Hojin Choi <hojin.choi@gmail.com>
2019-03-14 19:32:07 +0900

    Add error handling code

--- scripts/test_recycle_gan.sh
+++ scripts/test_recycle_gan.sh
@@ -16,7 +16,7 @@ PROJECT_LOC=Recycle-GAN
 DATA_LOC=../datasets/$NAMES
 cd $PROJECT_LOC
 #source activate recycle-gan
-python test.py --which_epoch $EPOCH --dataroot $DATA_LOC --name $NAMES --model cycle_gan  --which_model_netG resnet_6blocks   --dataset_mode unaligned  --no_dropout --gpu $GPU_ID  --how_many $NUM_TEST_IMAGES  --loadSize 256
+python test.py --which_epoch $EPOCH --dataroot $DATA_LOC --name $NAMES --model cycle_gan  --which_model_netG resnet_6blocks   --dataset_mode unaligned  --no_dropout --gpu $GPU_ID  --how_many $NUM_TEST_IMAGES  --loadSize 256 || exit
 
 cd .. 
 #source deactivate
