04934325a5911c1a4a2411c1c288c41445e00bd7
Hojin Choi <hojin.choi@gmail.com>
2019-03-19 08:05:58 +0900

    Add zeropad function, Add default CPUS calculation

--- scripts/common.sh
+++ scripts/common.sh
@@ -1,5 +1,7 @@
 
 _SKIP=0
+_CPUS=$(cat /proc/cpuinfo 2>/dev/null | grep processor | wc -l)
+_CPUS=${_CPUS:-4}
 wait_max() {
 	# ORDER: Check and wait child process without executing any external command in bash
 	if test $_SKIP -gt 0; then
@@ -8,8 +10,7 @@ wait_max() {
 	fi
 	MAXCHILDREN="$1"
 	if test $((MAXCHILDREN)) -eq 0; then
-		echo "Usage: wait_max <max children>"
-		echo "       wait if children count is over <max children>"
+		MAXCHILDREN=$_CPUS
 	fi
 
 	while true
@@ -22,4 +23,12 @@ wait_max() {
 		fi
 		read -t .2 x < /dev/zero
 	done
+}
+
+zeropad() {
+	DIGITS="$1"
+	NUMBER="$2"
+	local out
+	out="00000000000000000000000000$NUMBER"
+	echo ${out:0-$DIGITS}
 }
