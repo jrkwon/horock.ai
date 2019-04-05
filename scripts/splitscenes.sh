331ac665b6c10a1705ceab2fcb88a01a6a222d64
Hojin Choi <hojin.choi@gmail.com>
2019-04-05 15:15:14 +0900

    Add sub directory by 1000 images, Skip front 30 images of scenes

--- scripts/splitscenes.sh
+++ scripts/splitscenes.sh
@@ -4,6 +4,8 @@ INDIR="$1"
 OUTDIR="$2"
 PROBES="$3"
 
+MINFILESPERSCENE=1000
+
 . $(dirname $0)/common.sh
 
 if test -z "$INDIR" -o -z "$OUTDIR" -o -z "$PROBES"; then
@@ -11,6 +13,7 @@ if test -z "$INDIR" -o -z "$OUTDIR" -o -z "$PROBES"; then
 	exit 1
 fi
 
+COUNT=1
 while read LINE
 do
 	while read -d '|' KV
@@ -20,24 +23,35 @@ do
 			continue
 		fi
 		echo $ENDFRAME
+		let 'COUNT++'
 	done <<< "$LINE"
 done < "$PROBES" > "$PROBES.1"
 find "$INDIR" -name '[0-9]*.png' -print | awk -F/ '{print $NF}' | sort | tail -1 | tr -cd '0-9' >> "$PROBES.1"
+
+echo "Total $COUNT scenes. Now splitting..."
+SCENES=$(zeropad 4 $COUNT)
 
 DIRCOUNT=1
 COUNT=1
 NAME=$(basename $INDIR)
 while read ENDFRAME
 do
-	SUBDIR=$(printf "%04d" $DIRCOUNT)
-	echo "Scene #$SUBDIR ..."
+	SUBDIR=$(zeropad 4 $DIRCOUNT)
+	let 'FILES = ENDFRAME - COUNT + 1'
+	echo "Scene #$SUBDIR/$SCENES ($FILES files)..."
+	if test $FILES -lt $MINFILESPERSCENE; then
+		echo "Skip this scene($SUBDIR) just has $FILES files. (min. $MINFILESPERSCENE files required)"
+		let 'DIRCOUNT++'
+		let 'COUNT = ENDFRAME + 30'
+		continue
+	fi
 	mkdir -p "$OUTDIR/$SUBDIR/"
 	IMGFILES=""
 	while test $COUNT -le $ENDFRAME
 	do
 		IMGFILE="$(zeropad 6 $COUNT).png"
 		IMGFILES="$IMGFILES ../../$NAME/$IMGFILE"
-		let COUNT++
+		let 'COUNT++'
 		if test ${#IMGFILES} -gt 3000; then
 			echo "Linking ${IMGFILES:0:25} --> ${IMGFILES:0-24} to $OUTDIR/$SUBDIR/"
 			ln -sf $IMGFILES "$OUTDIR/$SUBDIR/" &
@@ -48,6 +62,6 @@ do
 		echo "Linking ${IMGFILES:0:25} --> ${IMGFILES:0-24} to $OUTDIR/$SUBDIR/"
 		ln -sf $IMGFILES "$OUTDIR/$SUBDIR/" &
 	fi
-	let DIRCOUNT++
+	let 'DIRCOUNT++'
 done < "$PROBES.1"
 
